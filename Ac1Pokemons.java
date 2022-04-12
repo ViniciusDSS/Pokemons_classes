27bdb5b60c1a8007ef43a97d70f26fc586ee4043
Vinicius Domingues Silva <Viniciusd37@outlook.com>
2022-04-12 18:05:13 -0300

    Corrigindo bugs

--- Ac1Pokemons.java
+++ Ac1Pokemons.java
@@ -16,24 +16,7 @@ public class Ac1Pokemons {
      
        pokemons MyPokemon = null;
      
-     
-    /*
-         System.out.println("Escolha um pokemon: \n (1) Charmander\n (2) Bulbasaur \n (3) Squirtle");
-         escolha = scan.nextInt();
-         
-         switch(escolha){
-             case 1: pokemons MyPokemon1 = new pokemons("Charmander", "Fogo", 1);
-             MyPokemon = MyPokemon1;
-             
-             break;
-             case 2: pokemons MyPokemon2 = new pokemons("Bulbasaur", "Planta", 1);
-             MyPokemon = MyPokemon2;
-             break;
-             case 3:  pokemons MyPokemon3 = new pokemons("Squirtle", "Agua", 1);
-             MyPokemon = MyPokemon3;
-             break;
-             
-         } */
+    
          
     pokemons InimigoPokemon = new pokemons("Pikachu", "Eletricidade", 1);
     pokemons InimigoPokemon2 = new pokemons("Magmar", "Fogo", 1);
@@ -55,25 +38,7 @@ public class Ac1Pokemons {
         System.out.println("Jogar mais um round? 1-Sim    2-Não\n");
         opcao = scan.nextInt();
     
-    switch(opcao){
-        case 1:
-            
-            System.out.println("Vamos para mais um round!!\n");
-            MyPokemon.setLevel(2);
     
-        System.out.println("Seu pokemon: ");
-        MyPokemon.imprimaAtributos();
-        System.out.println("Pokemon inimigo: ");
-        InimigoPokemon2.imprimaAtributos();
-        
-    newbatalha.myPokemon(MyPokemon.getNome(), (int) MyPokemon.getHp(), (int)MyPokemon.getHit(),(int)MyPokemon.getDef());
     
-    newbatalha.inimigoPokemon(InimigoPokemon.getNome(),(int)InimigoPokemon.getHp(), (int)InimigoPokemon.getHit(),(int)InimigoPokemon.getDef());
-    
-    newbatalha.batalhar();
-    break;
-        case 2: System.out.println("FIM de Jogo");
-    }
-    }
-    
+}
 }
