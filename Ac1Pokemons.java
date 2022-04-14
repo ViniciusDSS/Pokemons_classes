309d4d2f3245a9d657a68930a64612ced919ddbe
Vinicius Domingues Silva <Viniciusd37@outlook.com>
2022-04-13 21:32:24 -0300

    formatando código 2

--- Ac1Pokemons.java
+++ Ac1Pokemons.java
@@ -3,29 +3,14 @@ package ac1pokemons;
 import java.util.Scanner;
 
 public class Ac1Pokemons {
+    
+    
 
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
+       EscolherPokemons newescolha = new EscolherPokemons();
+       newescolha.setVisible(true);
 
-        pokemons MyPokemon = null;
-
-        pokemons InimigoPokemon = new pokemons("Pikachu", "Eletricidade", 1);
-        pokemons InimigoPokemon2 = new pokemons("Magmar", "Fogo", 1);
-
-        batalha newbatalha = new batalha();
-        System.out.println("Seu pokemon: ");
-        MyPokemon.imprimaAtributos();
-        System.out.println("Pokemon inimigo: ");
-        InimigoPokemon.imprimaAtributos();
-
-        newbatalha.myPokemon(MyPokemon.getNome(), (int) MyPokemon.getHp(), (int) MyPokemon.getHit(), (int) MyPokemon.getDef());
-
-        newbatalha.inimigoPokemon(InimigoPokemon.getNome(), (int) InimigoPokemon.getHp(), (int) InimigoPokemon.getHit(), (int) InimigoPokemon.getDef());
-
-        newbatalha.batalhar();
-
-        
 
     }
 }
